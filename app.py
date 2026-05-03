93e565b4ca21d4b14878463eb81f958c6b2fafcb
Vaishnav.v.v <vaishnavmadassery@gmail.com>
2026-05-03 12:13:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import tempfile, os
 from dotenv import load_dotenv
 load_dotenv()
 
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(page_title="DocChat AI", layout="centered")
 
 st.markdown("""
